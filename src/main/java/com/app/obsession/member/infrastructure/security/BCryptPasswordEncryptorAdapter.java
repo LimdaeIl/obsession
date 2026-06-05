106e62709adf1e6464afdd3cb4ecd4a6194f2f2e
LimdaeIl <optional94@naver.com>
2026-06-05 15:56:56 +0900

    ♻️#24: PasswordEncoder 네이밍 중복으로 인한 수정

---
+++ src/main/java/com/app/obsession/member/infrastructure/security/BCryptPasswordEncryptorAdapter.java
@@ -0,0 +1,23 @@
+package com.app.obsession.member.infrastructure.security;
+
+import com.app.obsession.member.application.port.PasswordEncryptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BCryptPasswordEncryptorAdapter implements PasswordEncryptor {
+
+    private final PasswordEncoder springPasswordEncoder;
+
+    @Override
+    public String encode(String rawPassword) {
+        return springPasswordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return springPasswordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
