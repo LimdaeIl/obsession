292201d56e126dc861a1d69c550e8e0dbd0048a5
LimdaeIl <optional94@naver.com>
2026-06-08 11:56:32 +0900

    ✨#42: 상품 재고 조회 구현

--- src/main/java/com/app/obsession/product/application/port/ProductStockRepository.java
+++ src/main/java/com/app/obsession/product/application/port/ProductStockRepository.java
@@ -1,9 +1,12 @@
 package com.app.obsession.product.application.port;
 
 import com.app.obsession.product.domain.ProductStock;
+import java.util.Optional;
 
 public interface ProductStockRepository {
 
     ProductStock save(ProductStock stock);
+
+    Optional<ProductStock> findByProductId(Long productId);
 
 }
--- src/main/java/com/app/obsession/product/infrastructure/persistence/JpaProductStockRepository.java
+++ src/main/java/com/app/obsession/product/infrastructure/persistence/JpaProductStockRepository.java
@@ -1,7 +1,11 @@
 package com.app.obsession.product.infrastructure.persistence;
 
 import com.app.obsession.product.domain.ProductStock;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductStockRepository extends JpaRepository<ProductStock, Long> {
+
+    Optional<ProductStock> findByProductId(Long productId);
 }
+
