6a8aa4c1b1ff2981ffb1b375532ff3b48633fed4
LimdaeIl <optional94@naver.com>
2026-06-10 13:43:26 +0900

    Revert "♻️#70: DTO 네이밍 개선"
    This reverts commit b5a9929e10465acfda8e60dbc2164df39ab08d6d.

--- src/main/java/com/app/obsession/order/presentation/dto/CancelPaidOrderRequest.java
+++ src/main/java/com/app/obsession/order/presentation/dto/CancelPaidOrderRequest.java
@@ -2,7 +2,7 @@ package com.app.obsession.order.presentation.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CancelOrderRequest(
+public record CancelPaidOrderRequest(
 
         @NotBlank
         String cancelReason
